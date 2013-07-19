22b2a516303d8fa90f2732d49b4bf98389031154
Steve Yen <steve.yen@gmail.com>
2013-07-19 10:07:21 -0700

    Revert "experimental mashup of cbgb, tuqtng, tuq_client"
    This reverts commit 48e2042cf86c0caaef0a2679f4572100d9353f3c.
    
    Reverted because it does not work on go 1.1.1, which seemingly does
    not allow treating a program (package main) as a package, something
    which works in go 1.1 on steve's machine.
    
    cbugg: bug-683

--- tuq_client/tuq_client.go
+++ tuq_client/tuq_client.go
@@ -11,60 +11,16 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"os"
 	"strings"
-	"time"
-
-	cbgb "github.com/couchbaselabs/cbgb"
-
-	tnetwork "github.com/couchbaselabs/tuqtng/network"
-	thttp "github.com/couchbaselabs/tuqtng/network/http"
-	tserver "github.com/couchbaselabs/tuqtng/server"
 )
 
 var tiServer = flag.String("tuqtng", "http://localhost:8093/", "URL to tuqtng")
 
 func main() {
-	flag.Usage = usage
 	flag.Parse()
-
-	tiServer := *tiServer
-	if tiServer == "play" {
-		cbgb.Main("embedded cbgb")
-		time.Sleep(2 * time.Second) // cbgb needs some time to start.
-		go tuqtng_play(":8093", "http://localhost:8091")
-		time.Sleep(1 * time.Second) // tuqtng needs some time to start.
-		tiServer = "http://localhost:8093/"
-	}
-
-	HandleInteractiveMode(tiServer)
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "%s <flags>\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "\nflags:\n")
-	fmt.Fprintf(os.Stderr, "  --tuqtng=URL\n")
-	fmt.Fprintf(os.Stderr, "    example: --tuqtng=http://localhost:8093/\n")
-	fmt.Fprintf(os.Stderr, "    if the URL is \"play\", then tuq_client runs a local\n")
-	fmt.Fprintf(os.Stderr, "    embedded server for developer playing convenience\n")
-}
-
-func tuqtng_play(addr, couchbase string) {
-	// create a QueryChannel
-	queryChannel := make(tnetwork.QueryChannel)
-
-	// create one or more network endpoints
-	httpEndpoint := thttp.NewHttpEndpoint(addr)
-	httpEndpoint.SendQueriesTo(queryChannel)
-
-	err := tserver.Server(couchbase, "default", queryChannel)
-	if err != nil {
-		log.Fatalf("Unable to run server, err: %v", err)
-	}
+	HandleInteractiveMode(*tiServer)
 }
 
 func execute_internal(tiServer, line string, w io.Writer) error {
