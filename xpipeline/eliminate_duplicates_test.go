7f9b9f6705dd50031f3932689b2fc5cf4cd8bb1b
Marty Schoch <marty.schoch@gmail.com>
2013-08-02 20:09:01 -0400

    got test coverage in xpipeline package back to respectable levels

---
+++ xpipeline/eliminate_duplicates_test.go
@@ -0,0 +1,54 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package xpipeline
+
+import (
+	"testing"
+
+	"github.com/couchbaselabs/dparval"
+)
+
+var duplicateTestData = dparval.ValueCollection{}
+
+func init() {
+	doc := dparval.NewValue(map[string]interface{}{
+		"name": "mike",
+	})
+	doc.SetAttachment("meta", map[string]interface{}{"id": "1"})
+	duplicateTestData = append(duplicateTestData, doc)
+
+	doc = dparval.NewValue(map[string]interface{}{
+		"name": "mike",
+	})
+	doc.SetAttachment("meta", map[string]interface{}{"id": "1"})
+	duplicateTestData = append(duplicateTestData, doc)
+}
+
+func TestEliminateDuplicates(t *testing.T) {
+
+	stubSource := NewStubSource(duplicateTestData)
+
+	ed := NewEliminateDuplicates()
+	ed.SetSource(stubSource)
+
+	edItemChannel, _ := ed.GetChannels()
+
+	go ed.Run()
+
+	count := 0
+	for _ = range edItemChannel {
+		count++
+	}
+
+	if count != 1 {
+		t.Errorf("Expected %d items, got %d", 1, count)
+	}
+
+}
---
+++ xpipeline/scan_test.go
@@ -0,0 +1,60 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+package xpipeline
+
+import (
+	"testing"
+
+	"github.com/couchbaselabs/tuqtng/catalog/mock"
+)
+
+func TestScan(t *testing.T) {
+
+	mocksite, err := mock.NewSite("mock:items=10")
+	if err != nil {
+		t.Errorf("Error creating mock site")
+	}
+	pool, err := mocksite.Pool("p0")
+	if err != nil {
+		t.Errorf("Error accessing pool p0")
+	}
+	bucket, err := pool.Bucket("b0")
+	if err != nil {
+		t.Errorf("Error accessing bucket b0")
+	}
+	scanner, err := bucket.Scanner("all_docs")
+	if err != nil {
+		t.Errorf("Error accessing scanner all_docs")
+	}
+
+	scan := NewScan(bucket, scanner)
+
+	scanItemChannel, _ := scan.GetChannels()
+
+	go scan.Run()
+
+	count := 0
+	for item := range scanItemChannel {
+		meta := item.GetAttachment("meta")
+		if meta == nil {
+			t.Errorf("Expected item to contain metadata")
+		}
+		_, ok := meta.(map[string]interface{})["id"]
+		if !ok {
+			t.Errorf("Expected metadata to contain an id")
+		}
+		count++
+	}
+
+	if count != 10 {
+		t.Errorf("Expected %d items, got %d", 10, count)
+	}
+
+}
