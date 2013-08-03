252785501f0a28cf756b7ab7a4aba56642e2a96d
Marty Schoch <marty.schoch@gmail.com>
2013-08-03 12:09:03 -0400

    added more tests for any/all collections

--- ast/collection_test.go
+++ ast/collection_test.go
@@ -26,8 +26,13 @@ func TestCollection(t *testing.T) {
 				"age": 18.0,
 			},
 		},
+		"emptyArray": []interface{}{},
+		"notAnArray": "bob",
 	}
 
+	propDNE := NewProperty("dne")
+	propEmpty := NewProperty("emptyArray")
+	propNot := NewProperty("notAnArray")
 	propChild := NewProperty("child")
 	propChildren := NewProperty("children")
 	propAge := NewProperty("age")
@@ -36,10 +41,26 @@ func TestCollection(t *testing.T) {
 	overThirty := NewGreaterThanOperator(pathChildDotAge, numberThirty)
 	anyChild := NewCollectionAnyOperator(overThirty, propChildren, "child")
 	allChild := NewCollectionAllOperator(overThirty, propChildren, "child")
+	anyEmpty := NewCollectionAnyOperator(overThirty, propEmpty, "child")
+	allEmpty := NewCollectionAllOperator(overThirty, propEmpty, "child")
+	anyNot := NewCollectionAnyOperator(overThirty, propNot, "child")
+	allNot := NewCollectionAllOperator(overThirty, propNot, "child")
+	anyDNE := NewCollectionAnyOperator(overThirty, propDNE, "child")
+	allDNE := NewCollectionAllOperator(overThirty, propDNE, "child")
+	anyMissingInside := NewCollectionAnyOperator(propDNE, propChildren, "child")
+	allMissingInside := NewCollectionAllOperator(propDNE, propChildren, "child")
 
 	tests := ExpressionTestSet{
 		{anyChild, true, nil},
 		{allChild, false, nil},
+		{anyEmpty, false, nil},
+		{allEmpty, true, nil},
+		{anyNot, false, nil},
+		{allNot, false, nil},
+		{anyDNE, false, nil},
+		{allDNE, false, nil},
+		{anyMissingInside, false, nil},
+		{allMissingInside, false, nil},
 	}
 
 	item := dparval.NewValue(sampleDocument)
