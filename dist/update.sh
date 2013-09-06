d2f883a881dda6dbb6c4994437308c0f660a61fc
Marty Schoch <marty.schoch@gmail.com>
2013-09-06 13:49:57 -0400

    removing packaging of old stand-alone tutorial

--- dist/update.sh
+++ dist/update.sh
@@ -49,22 +49,6 @@ buildclient() {
     wait
 }
 
-buildtutorial() {
-    pkg=$project/tutorial
-    goflags="-v -ldflags '-X main.VERSION $version'"
-
-    eval env GOARCH=386   GOOS=linux CGO_ENABLED=0 go build $goflags -o $DIST/tuq_tutorial.lin32 $pkg &
-    eval env GOARCH=arm   GOOS=linux CGO_ENABLED=0 go build $goflags -o $DIST/tuq_tutorial.arm $pkg &
-    eval env GOARCH=arm   GOARM=5 GOOS=linux CGO_ENABLED=0 go build $goflags -o $DIST/tuq_tutorial.arm5 $pkg &
-    eval env GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build $goflags -o $DIST/tuq_tutorial.lin64 $pkg &
-    eval env GOARCH=amd64 GOOS=freebsd CGO_ENABLED=0 go build $goflags -o $DIST/tuq_tutorial.fbsd $pkg &
-    eval env GOARCH=386   GOOS=windows go build $goflags -o $DIST/tuq_tutorial.win32.exe $pkg &
-    eval env GOARCH=amd64 GOOS=windows go build $goflags -o $DIST/tuq_tutorial.win64.exe $pkg &
-    eval env GOARCH=amd64 GOOS=darwin go build $goflags -o $DIST/tuq_tutorial.mac $pkg &
-
-    wait
-}
-
 builddistpackages() {
 
     mkdir -p $DIST/tutorial_tmp
@@ -155,17 +139,6 @@ builddistpackages() {
 compress() {
     rm -f $DIST/tuqtng.*.gz $DIST/tuq_client.*.gz $DIST/tuq_tutorial.*.gz || true
 
-    for i in lin32 arm arm5 lin64 fbsd win32 win64 mac
-    do
-        if [ -f $DIST/tuq_tutorial.$i ]; then
-            tar zcf $DIST/tuq_tutorial.$i.tar.gz    \
-              -C $top test/json/tutorial \
-              -C $top/tutorial content \
-              -C $DIST tuq_tutorial.$i
-            rm -f $DIST/tuq_tutorial.$i
-        fi &
-    done
-
     for i in $DIST/tuqtng.* $DIST/tuq_client.*
     do
         gzip -9v $i &
@@ -204,7 +177,6 @@ testpkg
 mkversion
 build
 buildclient
-buildtutorial
 builddistpackages
 compress
 benchmark
