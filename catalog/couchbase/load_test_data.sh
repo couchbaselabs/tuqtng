5bed7101c9dcfd3d67d3dff7ffa50f5c2e1c016d
Dustin Sallings <dustin@spy.net>
2013-11-20 11:06:58 -0800

    add -s to curl in loader to supress output

--- catalog/couchbase/load_test_data.sh
+++ catalog/couchbase/load_test_data.sh
@@ -4,7 +4,7 @@ for file in test/json/*
     do
       if [ -d "$file" ];then
        bn=`basename "$file"`
-       curl -XPOST "http://localhost:8091/_api/buckets" -d "name=$bn"
+       curl -s -XPOST "http://localhost:8091/_api/buckets" -d "name=$bn"
        loadfile -bucket $bn -v test/json/$bn/*
       fi
     done
