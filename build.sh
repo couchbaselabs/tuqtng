44c8978f21ed9bad58e626b59b64812cc033859c
manik <manik@couchbase.com>
2013-12-04 20:21:51 +0530

    Add some more flags to build.sh (fixed)

--- build.sh
+++ build.sh
@@ -2,13 +2,24 @@
 
 if [ -z "$1" ]
     then
-    echo "echo building cbq-engine"
+    echo "building cbq-engine"
     go build -o cbq-engine
+elif [ $1 == "debug" ]
+    then
+    echo "building cbq-engine with debug flags"
+    go build -gcflags "-N -l" -o cbq-engine
+elif [ $1 == "release" ]
+    then
+    echo "building cbq-engine without debug symbols"
+    go build -ldflags "-s" -o cbq-engine
 elif [ $1 == "clean" ]
     then
-    echo "cleaning cbq-engine"
+    echo "cleaning.. "
     go clean
     rm -f cbq-engine
+elif [ $1 == "fmt" ]
+    then
+    go fmt ./...
 else
     echo "No comprehendo senor !"
 fi
