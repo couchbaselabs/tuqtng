d2d782065b16c3b20c8a828715a4878a929f67e4
geraldss <gerald@couchbase.com>
2014-01-10 09:51:38 -0800

    Build both cbq-engine and cbq/cbq.

--- build.sh
+++ build.sh
@@ -2,21 +2,25 @@
 
 if [ -z "$1" ]
     then
-    echo "building cbq-engine"
+    echo "building cbq-engine and cbq/cbq"
     go build -o cbq-engine
+    cd cbq; go build; cd ..
 elif [ $1 == "debug" ]
     then
-    echo "building cbq-engine with debug flags"
+    echo "building cbq-engine and cbq/cbq with debug flags"
     go build -gcflags "-N -l" -o cbq-engine
+    cd cbq; go build -gcflags "-N -l"; cd ..
 elif [ $1 == "release" ]
     then
-    echo "building cbq-engine without debug symbols"
+    echo "building cbq-engine and cbq/cbq without debug symbols"
     go build -ldflags "-s" -o cbq-engine
+    cd cbq; go build -ldflags "-s"; cd ..
 elif [ $1 == "clean" ]
     then
     echo "cleaning.. "
     go clean
     rm -f cbq-engine
+    cd cbq; rm -f cbq; cd ..
 elif [ $1 == "fmt" ]
     then
     go fmt ./...
