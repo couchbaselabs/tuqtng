c7647afb4c0ae2a8469780de5ae587de07620194
Sriram Melkote <siri@couchbase.com>
2014-04-07 11:14:32 +0530

    Make tutorial wd independent

--- dist/start_tutorial.sh
+++ dist/start_tutorial.sh
@@ -4,4 +4,5 @@ echo
 echo "In your web browser open http://localhost:8093/tutorial/"
 echo
 
-./cbq-engine -couchbase dir:./data
+WD="`dirname $0`"
+$WD/cbq-engine -couchbase dir:$WD/data -staticPath $WD/static
